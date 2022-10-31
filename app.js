dcb57c5996b3091f870f105e48df70d6269f37a7
Juan Serrano <109055683+JuanDserrano727@users.noreply.github.com>
2022-10-31 18:44:45 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+let redBtn1 = document.getElementById("one");
+let blueBtn1 = document.getElementById("two");
+let blackBtn1 = document.getElementById("three");
+let imgchange1 = document.getElementById("imgchange1");
+
+redBtn1.onclick = function() {
+
+    imgchange1.src = "./imagenes/productos/labial1.jpg";
+}
+
+blueBtn1.onclick = function() {
+    imgchange1.src = "./imagenes/productos/labial2.jpg";
+}
+
+blackBtn1.onclick = function() {
+    imgchange1.src = "./imagenes/productos/labial3.jpg";
+}
+
+
+let redBtn2 = document.getElementById("four");
+let blueBtn2 = document.getElementById("five");
+let blackBtn2 = document.getElementById("six");
+let imgchange2 = document.getElementById("imgchange2");
+
+redBtn2.onclick = function() {
+
+    imgchange2.src = "./imagenes/productos/pes1.jpg";
+}
+
+blueBtn2.onclick = function() {
+    imgchange2.src = "./imagenes/productos/pes2.jpg";
+}
+
+blackBtn2.onclick = function() {
+    imgchange2.src = "./imagenes/productos/pes3.jpg";
+}
+
+
+let redBtn3 = document.getElementById("seven");
+let blueBtn3 = document.getElementById("eight");
+let blackBtn3 = document.getElementById("nine");
+let imgchange3 = document.getElementById("imgchange3");
+
+redBtn3.onclick = function() {
+
+    imgchange3.src = "./imagenes/productos/pestañas1.jpg";
+}
+
+blueBtn3.onclick = function() {
+    imgchange3.src = "./imagenes/productos/pestañas2.jpg";
+}
+
+blackBtn3.onclick = function() {
+    imgchange3.src = "./imagenes/productos/pestañas3.jpg";
+}
+
+
+let redBtn4 = document.getElementById("ten");
+let blueBtn4 = document.getElementById("eleven");
+let blackBtn4 = document.getElementById("twelve");
+let imgchange4 = document.getElementById("imgchange4");
+
+redBtn4.onclick = function() {
+
+    imgchange4.src = "./imagenes/productos/pinceles1.jpg";
+}
+
+blueBtn4.onclick = function() {
+    imgchange4.src = "./imagenes/productos/pinceles2.jpg";
+}
+
+blackBtn4.onclick = function() {
+    imgchange4.src = "./imagenes/productos/pinceles3.jpg";
+}
